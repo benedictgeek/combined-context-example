0d834bc14bf6ab1c5ab546a7e43b3e9b6af3399e
Olushola <olushola251@gmail.com>
2022-02-20 19:03:28 +0100

    updated example with animated thumbs up

--- src/child.js
+++ src/child.js
@@ -1,4 +1,5 @@
 import { useContextTwo } from "./state/context2";
+import { Reaction } from "reaction-react";
 
 let Child = (props) => {
   let {
@@ -8,6 +9,13 @@ let Child = (props) => {
     <div style={{ padding: "15px" }}>
       The body content is rendered below
       <i style={{ marginLeft: "10px" }}>{content}</i>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span style={{ marginRight: "15px" }}>Give a thumbs up</span>
+
+        <Reaction>
+          <span style={{ cursor: "pointer", fontSize: "30px" }}>👍</span>
+        </Reaction>
+      </div>
     </div>
   );
 };
